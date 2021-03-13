b61705980020290461023e70ce8a2a696e8a9d39
Juan <juanmateuz@gmail.com>
2021-03-13 00:20:07 -0500

    Initial Commit

---
+++ Infraestructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
